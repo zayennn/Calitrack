283de6ea1db872713a6728ab655dfc13f7bfb456
zayennn <zaayeennn@gmail.com>
2026-07-23 13:24:58 +0700

    update

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String home = '/home';
+  static const String workout = '/workout';
+  static const String workoutDetail = '/workout-detail';
+  static const String progress = '/progress';
+  static const String weightTracker = '/weight-tracker';
+  static const String calendar = '/calendar';
+  static const String statistics = '/statistics';
+  static const String settings = '/settings';
+}
